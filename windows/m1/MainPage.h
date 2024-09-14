28dbef754c97602256be10af0183e97a49094dc9
yarosalv chorniy <yaroslavert02@gmail.com>
2024-09-15 02:05:12 +0300

    added windows support

---
+++ windows/m1/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::m1::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::m1::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
